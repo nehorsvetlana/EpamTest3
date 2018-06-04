bb6459ee3d3616cc815f4ea78c5b4c0da1d03b97
nehor <nehorsvetlana@gmail.com>
2018-06-04 17:46:51 +0300

    HW5

--- src/test/java/MySelenideTest.java
+++ src/test/java/MySelenideTest.java
@@ -35,9 +35,9 @@ public class MySelenideTest {
         //driver = new ChromeDriver();
         //driver.manage().window().maximize();//размер окна
         Configuration.browser = "chrome";
-        Configuration.startMaximized = true;
+        //Configuration.startMaximized = true;
         //Configuration.timeout = 50000;
-        //Configuration.browserSize="700x700";
+        Configuration.browserSize="700x500";
         Configuration.selectorMode = Sizzle; //Теперь в Selenide можно использовать CSS3 селекторы для поиска элементов!
         EpamSelenid.init();
     }
