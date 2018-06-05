147aaa4268871d25bb723d28fa68c0a9f64cac05
nehor <nehorsvetlana@gmail.com>
2018-06-05 08:51:56 +0300

    HW5

--- src/test/java/MySelenideTest.java
+++ src/test/java/MySelenideTest.java
@@ -36,6 +36,7 @@ public class MySelenideTest {
         //driver = new ChromeDriver();
         //driver.manage().window().maximize();//размер окна
         Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080x24";
         Configuration.startMaximized = true;
         //Configuration.timeout = 50000;
         //Configuration.browserSize="700x700";
@@ -46,7 +47,7 @@ public class MySelenideTest {
     @Test
     public void Case1() {
         //HomePage
-        //zoom(0.80);
+        zoom(0.5);
         EpamSelenid.homePage.open();
         EpamSelenid.homePage.login(LOGIN.toString(), PASSWORD.toString());
         EpamSelenid.homePage.checkUserLogIn(USER_NAME.toString());
